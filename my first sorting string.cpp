b78a5816027da4c19e81f1d0b299fa22232d5e54
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-05-26 01:45:34 +0600

    Create my first sorting string.cpp

---
+++ my first sorting string.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ int main(){
+     int x,y;
+     
+     cout<< "Enter the value of integer x :";
+     cin >> x;
+     cout<< "Enter the value of integer y:";
+     cin >> y;
+     
+     cout <<"minumum "<<  min ( x,y)  <<  "  maximum " <<  max ( x,y) << endl;
+     return 0;
+ }
